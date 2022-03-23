1aa5d4e1ce55e14eddfe5c89df2d83822c56abe8
brian-wickens <97416931+brian-wickens@users.noreply.github.com>
2022-03-23 17:26:00 -0400

    Added scripts for each analysis

---
+++ EUI vs building age regression testing.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 23 17:10:40 2022
+
+@author: Brian
+
+To tests if there are a significant correlation between building age and energy 
+intensity among different categories of buildings and build regression models.
+
+Notes:
+Are there other variables which we can use to make this MLR?
+Check regression assumptions
+If we can identify timelines for new-build standards in city or building code 
+can run t-test for buildings pre/post design requirement changes.
+"""
+
